6c80cf7228c1df4cd0db41db6b634593b4703aa7
avokjart <slobodance15@gmail.com>
2023-05-08 13:13:20 +0200

    Lade till en test som inte gick igenom

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -1,19 +1,29 @@
 const stack = require('../src/stack');
 
 test('peek on empty stack returns undefined', () => {
-    expect(stack.peek()).toBeUndefined();
+  expect(stack.peek()).toBeUndefined();
 });
 
 test('peek on stack with one element returns that element', () => {
-    stack.push(1);
-    expect(stack.peek()).toBeDefined();
-    expect(stack.peek()).toBe(1);
+  stack.push(1);
+  expect(stack.peek()).toBeDefined();
+  expect(stack.peek()).toBe(1);
 });
 
 test('peek on stack with two or more elements returns the top element', () => {
-    stack.push(1);
-    stack.push("wow");
-    stack.push(42);
-    expect(stack.peek()).toBeDefined();
-    expect(stack.peek()).toBe(42);
+  stack.push(1);
+  stack.push("wow");
+  stack.push(42);
+  expect(stack.peek()).toBeDefined();
+  expect(stack.peek()).toBe(42);
+});
+
+
+test('pop() removes and returns top element from the stack', () => {
+  stack.push('a');
+  stack.push('b');
+  stack.push('c');
+  const poppedElement = stack.pop();
+  expect(poppedElement).toBe('b');
+  expect(stack.peek()).toBe('c');
 });
